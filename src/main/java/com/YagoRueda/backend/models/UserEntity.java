07dd3eb83593f3cb37f38126afa1ceeb8e06bdd1
yago-vasco <yago.rc.04@gmail.com>
2025-07-22 20:04:55 +0200

    build(auth): modificación de la entidad de usuario para añadir la lista de tokens

--- src/main/java/com/YagoRueda/backend/models/UserEntity.java
+++ src/main/java/com/YagoRueda/backend/models/UserEntity.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class UserEntity {
     @Id
@@ -21,6 +24,11 @@ public class UserEntity {
     @Getter
     @Setter
     private Instant signup_date;
+
+    @Setter
+    @Getter
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TokenEntity> tokens = new ArrayList<>();
 
 
 }
